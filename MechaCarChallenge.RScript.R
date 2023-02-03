a251d89b0d5470ad58920c9c6a0f29d64a954d74
tylerfallon <tylermfallon@gmail.com>
2023-02-02 23:25:59 -0500

    updated R file

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,6 +1,10 @@
-library(dplyr)
+## Deliverable 1
 
-mechacar_mpg <- read.csv(file="./Resources/MechaCar_mpg.csv")
+library(dplyr)
+library(tidyverse)
+
+
+mechacar_mpg <- read.csv(file="./Resources/MechaCar_mpg.csv", check.names=F,stringsAsFactors = F)
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg)
 
@@ -8,4 +12,7 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 lm(mpg ~ vehicle_length + ground_clearance, data=mechacar_mpg)
 summary(lm(mpg ~ vehicle_length + ground_clearance, data=mechacar_mpg))
+
+## Deliverable 2
+suspension_coil <- read.csv(file='./Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
 
